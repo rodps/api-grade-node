fc06e38412b88f5982565a70364e54bd54916469
Rodrigo da Silva <rodrigopds73@gmail.com>
2023-12-29 18:48:24 -0300

    remove cors configuration

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,7 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT ?? 3000
 
-app.use(
-  cors({
-    origin: "*"
-  })
-)
+app.use(cors())
 app.use(express.json())
 
 app.use(produtoRouter)
